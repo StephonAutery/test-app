8a4f8c918e39ab8a73a4d60a7fe0ceb4e496fdaa
Stephon Autery <stephon@stephonautery.com>
2024-03-20 19:29:21 -0700

    firt big update

---
+++ util/scripts/header.js
@@ -0,0 +1,10 @@
+// const template = document.createElement("template");
+const header = document.getElementById("header");
+header.innerHTML = `
+<div class="header">
+  <a href="./index.html">home</a>
+  <a href="./tiles.html">tiles</a>
+</div>
+`;
+
+// document.body.appendChild(template.content);
